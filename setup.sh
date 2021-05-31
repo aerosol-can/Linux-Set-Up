b3c8c339d4e47151f536be2f26e93a9cbc5e86ce
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2021-06-01 00:27:41 +0530

    let's test this

--- setup.sh
+++ setup.sh
@@ -9,8 +9,11 @@ apt install zsh git
 #install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 
+cd ~
+
 #clone powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 
- #copying the .zshrc file
+
+#copying the .zshrc file
 curl https://raw.githubusercontent.com/aerosol-can/Linux-Set-Up/main/.zshrc > .zshrc 
